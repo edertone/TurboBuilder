dfae3a1e2109aed1bcdf5f2a00b4a6e3875d0e25
Jaume Mussons <jaumemussons79@gmail.com>
2017-04-06 20:26:08 +0200

    Implemented php namespace validation rule and minor refactors

--- TurboBuilder-Ant/turboBuilder/Validate.js
+++ TurboBuilder-Ant/turboBuilder/Validate.js
@@ -3,8 +3,8 @@
  */
 
 
-// Import Validate Utils
-load(project.getProperty("basedir") + '/ValidateUtils.js');
+// Import Utils
+load(project.getProperty("basedir") + '/Utils.js');
 
 
 /** Array that will contain all the warnings detected by this script and will be displayed at the end */
@@ -13,12 +13,43 @@ var antWarnings = [];
 /** Array that will contain all the errors detected by this script and will be displayed at the end */
 var antErrors = [];
 
+/** Full paths to the most common project folders */
+var projectBaseDir = project.getProperty("basedir") + "/../";
+var projectSrcDir = projectBaseDir + '/src';
+
 
 //--------------------------------------------------------------------------------------------------------------------------------------
 // Apply the ProjectStructure rule if enabled
 if(project.getProperty("Validate.ProjectStructure.enabled") === "true"){
 	
 	// TODO
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+//Apply the PhpNamespaces rule if enabled
+if(project.getProperty("Validate.PhpNamespaces.enabled") === "true"){
+		
+	var phpFiles = getFilesList(projectSrcDir, "**/*.php");
+	
+	for(var i = 0; i < phpFiles.length; i++){
+		
+		var file = loadFileAsString(projectSrcDir + "/" + phpFiles[i]);
+		
+		if(file.indexOf("namespace") >= 0){
+
+			var fileNamespace = file.split("namespace")[1].split(";")[0];
+			
+			var namespace = phpFiles[i].split('\\');
+			namespace.pop();
+			namespace = namespace.join('\\');
+			
+			if(fileNamespace.indexOf(namespace) < 0){
+				
+				antErrors.push(phpFiles[i] + " namespace <" + fileNamespace + "> is invalid. Must contain <" + namespace + ">");
+			}
+		}
+	}		
 }
 
 
@@ -34,7 +65,6 @@ if(project.getProperty("Validate.Css.enabled") === "true"){
 // Apply the CopyrightHeaders rule if enabled
 if(project.getProperty("Validate.CopyrightHeaders.enabled") === "true"){
 	
-	var baseDir = project.getProperty("basedir") + "/../";
 	var paths = project.getProperty("Validate.CopyrightHeaders.Header").split(",");
 	var appliesTo = project.getProperty("Validate.CopyrightHeaders.Header.appliesTo").split(",");
 	var includes = project.getProperty("Validate.CopyrightHeaders.Header.includes").split(",");
@@ -42,13 +72,13 @@ if(project.getProperty("Validate.CopyrightHeaders.enabled") === "true"){
 	
 	for(var i = 0; i < paths.length; i++){
 		
-		var header = loadFileAsString(baseDir + paths[i]);
+		var header = loadFileAsString(projectBaseDir + paths[i]);
 		
-		var files = getFilesList(baseDir + appliesTo[i], includes[i], excludes[i]);
+		var files = getFilesList(projectBaseDir + appliesTo[i], includes[i], excludes[i]);
 		
 		for(var j = 0; j < files.length; j++){
 			
-			var file = loadFileAsString(baseDir + appliesTo[i] + "/" + files[j]);
+			var file = loadFileAsString(projectBaseDir + appliesTo[i] + "/" + files[j]);
 			
 			if(file.indexOf(header) != 0){
 				
