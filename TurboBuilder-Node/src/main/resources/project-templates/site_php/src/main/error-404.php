629bac553e24240fbc53084812255c29f9f51898
Jaume Mussons <jaume.mussons@edertone.com>
2018-05-22 22:01:44 +0200

    Improved error 404 page and robots.txt

--- TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/error-404.php
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/error-404.php
@@ -1,37 +1,71 @@
-<?php
-
-/**
- * TurboCommons is a general purpose and cross-language library that implements frequently used and generic software development tasks.
- *
- * Website : -> http://www.turbocommons.org
- * License : -> Licensed under the Apache License, Version 2.0. You may not use this file except in compliance with the License.
- * License Url : -> http://www.apache.org/licenses/LICENSE-2.0
- * CopyRight : -> Copyright 2015 Edertone Advanded Solutions (08211 Castellar del Vallès, Barcelona). http://www.edertone.com
- */
-
-?>
-
-<!DOCTYPE html>
-<html>
-
+<!doctype html>
+<html lang="en">
 <head>
-	<title>Error 404 page not found</title>
+    <meta charset="utf-8">
+    <title>Error 404 page not found</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+
+        * {
+            line-height: 1.2;
+            margin: 0;
+        }
+
+        html {
+            color: #888;
+            display: table;
+            font-family: sans-serif;
+            height: 100%;
+            text-align: center;
+            width: 100%;
+        }
+
+        body {
+            display: table-cell;
+            vertical-align: middle;
+            margin: 2em auto;
+        }
+
+        h1 {
+            color: #555;
+            font-size: 2em;
+            font-weight: 400;
+        }
+
+        p {
+            margin: 0 auto;
+            width: 280px;
+        }
+
+        @media only screen and (max-width: 280px) {
+
+            body, p {
+                width: 95%;
+            }
+
+            h1 {
+                font-size: 1.5em;
+                margin: 0 0 0.3em;
+            }
+
+        }
+
+    </style>
 </head>
-
 <body>
-<?php
+    <h1>Page Not Found</h1>
+    <p>Sorry, but the page you were trying to view does not exist.</p>
+	<br>
+	<p>Redirecting to home in <span id="time">6</span> secs ...</p>
 
-	// TODO: el missatge hauria de ser localitzat en els idiomes més frequents, i si no angles per defecte
-	echo 'Sorry, this page was not found (404 error). Redirecting to home in <p id="time" style="display:inline">5</p> secs ...';
-
-?>
-<script>
-	setTimeout(function(){document.getElementById('time').innerHTML="4"},1000);
-	setTimeout(function(){document.getElementById('time').innerHTML="3"},2000);
-	setTimeout(function(){document.getElementById('time').innerHTML="2"},3000);
-	setTimeout(function(){document.getElementById('time').innerHTML="1"},4000);
-	setTimeout(function(){window.location.href="/"}, 5000);
-</script>
-
+	<script>
+		setTimeout(function(){document.getElementById('time').innerHTML="5"},1000);
+		setTimeout(function(){document.getElementById('time').innerHTML="4"},2000);
+		setTimeout(function(){document.getElementById('time').innerHTML="3"},3000);
+		setTimeout(function(){document.getElementById('time').innerHTML="2"},4000);
+		setTimeout(function(){document.getElementById('time').innerHTML="1"},5000);
+		setTimeout(function(){window.location.href="/"}, 6000);
+	</script>
 </body>
 </html>
+<!-- IE needs 512+ bytes: https://blogs.msdn.microsoft.com/ieinternals/2010/08/18/friendly-http-error-pages/ -->
