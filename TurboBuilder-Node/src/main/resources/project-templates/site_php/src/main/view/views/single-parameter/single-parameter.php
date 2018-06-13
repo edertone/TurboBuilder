93e56cbed16736a909797b247fc559aeacaad9da
Jaume Mussons <jaume.mussons@edertone.com>
2018-06-13 20:21:19 +0200

    Improved single parameter view

--- TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/view/views/single-parameter/single-parameter.php
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/view/views/single-parameter/single-parameter.php
@@ -4,7 +4,7 @@ use org\turbosite\src\main\php\model\WebSite;
 
 $ws = WebSite::getInstance();
 
-$ws->initializeSingleParameterView();
+$ws->initializeSingleParameterView('en', ['some parameter', 'testparameter']);
 
 $ws->metaTitle = $ws->getParam();
 $ws->metaDescription = $ws->getParam();
@@ -19,9 +19,25 @@ $ws->metaDescription = $ws->getParam();
 
 <body>
 
-	<h1><?php echo 'This is the single parameter view' ?></h1>
+    <?php $ws->includeComponent('view/components/main-menu/main-menu') ?>
 
-    <p><?php echo 'Use it to process data when only one parameter is enough. The only parameter restriction is to be more than 2 digits in length' ?></p>
+    <main>
+
+        <section>
+
+            <h1><?php echo 'This is the single parameter view' ?></h1>
+
+            <h4><?php echo 'You passed the following parameter value: '.$ws->getParam() ?></h4>
+
+            <p><?php echo 'Use it to process data when only one parameter is enough. The only parameter restriction is to be more than 2 digits in length' ?></p>
+
+        </section>
+
+    </main>
+
+    <?php $ws->includeComponent('view/components/footer/footer') ?>
+
+    <?php $ws->echoJavaScriptTags() ?>
 
 </body>
 
