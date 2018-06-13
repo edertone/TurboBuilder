1e341dacf52b34d5358338d3732efd7fbf9dbd92
Jaume Mussons <jaume.mussons@edertone.com>
2018-06-13 19:10:36 +0200

    Added locale change links to site_php template

--- TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/view/components/footer/footer.php
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/view/components/footer/footer.php
@@ -15,4 +15,10 @@ $ws->loadBundles(['footer']);
     <p><?php $ws->echoLoc('DOC_GENERATED_IN_N_SECS',
             ['bundle' => 'footer', 'wildcards' => '$N', 'replace' => $ws->getRunningTime()]) ?></p>
 
+    <p>
+        <a href="<?php $ws->echoUrlToChangeLocale('en_US') ?>">english</a>
+         -
+        <a href="<?php $ws->echoUrlToChangeLocale('es_ES') ?>">spanish</a>
+    </p>
+
 </footer>
