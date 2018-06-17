a9970c949ea39334bcb07c930898240ba36c9cdb
Jaume Mussons <jaume.mussons@edertone.com>
2018-06-17 15:57:18 +0200

    Implemented multi parameters view on sitephp template

--- TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/view/views/multi-parameters/multi-parameters.php
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/view/views/multi-parameters/multi-parameters.php
@@ -4,26 +4,42 @@ use org\turbosite\src\main\php\model\WebSite;
 
 $ws = WebSite::getInstance();
 
-$ws->initializeView();
+$ws->initializeView(3);
+
+$ws->loadBundles(['multi-parameters']);
+
+$ws->loadComponents([
+    'view/components/main-menu',
+    'view/components/footer']);
+
+$ws->metaTitle = $ws->getLoc('META_TITLE');
+$ws->metaDescription = $ws->getLoc('META_DESCRIPTION');
 
 ?>
 <!doctype html>
 <html lang="<?php echo $ws->getPrimaryLanguage() ?>">
 
 <head>
-    <?php $ws->echoHeadHtml() ?>
+<?php $ws->echoHeadHtml() ?>
 </head>
 
 <body>
 
-	<?php //(new Header())->echoHTML() ?>
+    <?php $ws->includeComponent('view/components/main-menu/main-menu') ?>
+
+    <main>
+
+        <section>
 
 
-	<!-- Place HTML BODY contents here -->
 
+        </section>
 
-	<?php //(new Footer())->echoHTML() ?>
+    </main>
+
+    <?php $ws->includeComponent('view/components/footer/footer') ?>
+
+    <?php $ws->echoJavaScriptTags() ?>
 
 </body>
-
 </html>
