ab514c46412fb33cea6f1fada55f140bc849fd89
Jaume Mussons <jaume.mussons@edertone.com>
2018-08-21 20:07:53 +0200

    Added php minimal version verification

--- TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/index.php
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/index.php
@@ -11,13 +11,20 @@ if(file_exists('cache/hash')){
     die();
 }
 
+// Check server version
+if (version_compare(PHP_VERSION, '7.1.0', '<')) {
+    
+    // TODO - throw exception instead of echoing the error message, and make
+    // sure that it is captured by the error management
+    echo 'PHP 7.1.0 required but found '.PHP_VERSION;
+    die();
+}
+
 require 'autoloader.php';
 require 'libs/turbocommons-php/TurboCommons-Php-0.7.2.phar';
 require 'libs/turbosite/TurboSite-Php-0.2.0.phar';
 
-use org\turbosite\src\main\php\model\WebSite;
-
-$ws = WebSite::getInstance();
+$ws = org\turbosite\src\main\php\model\WebSite::getInstance();
 
 $ws->initialize(__FILE__);
 
