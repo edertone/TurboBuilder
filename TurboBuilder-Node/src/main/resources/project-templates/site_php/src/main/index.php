cbe69f486c703a756fcdaee0a1c57f8e0c91a41d
Jaume Mussons <jaume.mussons@edertone.com>
2018-08-25 13:13:26 +0200

    Removed php version validation from index.php
    The turbosite phar will take care of this verification on the
    autoloader.php file

--- TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/index.php
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/index.php
@@ -11,12 +11,6 @@ if(file_exists('cache/hash')){
     die();
 }
 
-// Check server version
-if (version_compare(PHP_VERSION, '7.1.0', '<')) {
-
-    die('PHP 7.1.0 required but found '.PHP_VERSION);
-}
-
 require 'autoloader.php';
 require 'libs/turbocommons-php/TurboCommons-Php-0.7.2.phar';
 require 'libs/turbosite/TurboSite-Php-0.2.0.phar';
