522cc345423cac12d961f88689b507105193d6c7
Jaume Mussons <jaume.mussons@edertone.com>
2018-08-28 16:29:09 +0200

    Minor code cleanup

--- TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/api/service.php
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/api/service.php
@@ -10,32 +10,6 @@
  */
 
 
-use com\edertone\turboProject\src\main\php\model\App;
-
-
-App::initializeView();
-// App::importCss('css/view/views/home/Home.css');
+// TODO - implement the service structure
 
 ?>
-<!DOCTYPE html>
-<html lang="<?php echo App::getCurrentLanguage() ?>">
-
-<head>
-<?php
-	App::echoHeadHtml();
-?>
-</head>
-
-<body>
-
-	<?php //(new Header())->echoHTML() ?>
-
-
-	<!-- Place HTML BODY contents here -->
-
-
-	<?php //(new Footer())->echoHTML() ?>
-
-</body>
-
-</html>
