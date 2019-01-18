16b6da47f93131a07d905348ca7bdd008523de7e
Jaume Mussons <jaume.mussons@edertone.com>
2019-01-18 12:08:05 +0100

    Added example rest services to site_php template

---
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/api/site/example/ExampleServiceWithGetParams.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace project\src\main\api\site\example;
+
+use org\turbosite\src\main\php\model\WebService;
+
+
+/**
+ * An example of a service with get parameters
+ */
+class ExampleServiceWithGetParams extends WebService{
+
+
+    public function setup(){
+
+        // We will accept two get parameters
+        $this->enabledGetParams = 2;
+    }
+
+
+    public function run(){
+
+        $result = [
+            "info" => "this object is returned as a json string with the received GET parameters values",
+            "received-param-0-value" => $this->getParam(0),
+            "received-param-1-value" => $this->getParam(1)
+        ];
+
+        return json_encode($result);
+    }
+}
+
+?>
---
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/api/site/example/ExampleServiceWithoutParams.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace project\src\main\api\site\example;
+
+use org\turbosite\src\main\php\model\WebService;
+
+
+/**
+ * An example of a service without any parameter
+ */
+class ExampleServiceWithoutParams extends WebService{
+
+
+    public function setup(){
+
+        // Nothing is changed here
+    }
+
+
+    public function run(){
+
+        return 'Any value can be output by the service as a string (json or xml data, plain text, etc..)';
+    }
+
+}
+
+?>
