d38fff86e4c2f2cb830f3f6e40555157aa0b8a9f
Jaume Mussons <jaume.mussons@edertone.com>
2019-09-08 11:36:53 +0200

    Fixed lib_php template resources folder

--- TurboBuilder-Node/src/main/resources/project-templates/lib_php/src/test/php/autoloader.php
+++ TurboBuilder-Node/src/main/resources/project-templates/lib_php/src/test/php/autoloader.php
@@ -11,12 +11,12 @@ spl_autoload_register(function($className){
     $classPath = str_replace('\\', DIRECTORY_SEPARATOR, str_replace('/', DIRECTORY_SEPARATOR, $className));
 
     // Remove unwanted classname path parts
-    $classPath = explode('src'.DIRECTORY_SEPARATOR.'test'.DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR, $classPath);
+    $classPath = explode('src'.DIRECTORY_SEPARATOR.'test'.DIRECTORY_SEPARATOR, $classPath);
     $classPath = array_pop($classPath).'.php';
 
-    if(file_exists(__DIR__.DIRECTORY_SEPARATOR.$classPath)){
+    if(file_exists(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.$classPath)){
 
-        require_once __DIR__.DIRECTORY_SEPARATOR.$classPath;
+        require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.$classPath;
     }
 });
 
