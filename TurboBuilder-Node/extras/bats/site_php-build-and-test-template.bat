a1f74d28dbbb39f847c803fd0e443b43760cfc01
Jaume Mussons <jaume.mussons@edertone.com>
2019-09-27 22:41:18 +0200

    MINOR: Improved automate-site_php-build-and-test bat file template

---
+++ TurboBuilder-Node/extras/bats/site_php-build-and-test-template.bat
@@ -0,0 +1,58 @@
+:: This file contains a template to automate the process of building a generated site_php
+:: project, including required turbo dependencies and launching the respective tests.
+:: Modify it to fit your needs
+
+
+@SET DESKTOP_PATH=%USERPROFILE%\Desktop
+@SET TURBOSITE_PHP_PATH=%USERPROFILE%\git\TurboSite\TurboSite-Php
+@SET TURBOCOMMONS_PHP_PATH=%USERPROFILE%\git\TurboCommons\TurboCommons-Php
+@SET TURBODEPOT_PHP_PATH=%USERPROFILE%\git\TurboDepot\TurboDepot-Php
+@SET WEB_SERVER_DEV_FOLDER_PATH=C:\turbosite-webserver-symlink\_dev
+
+
+:: Create a work directory into the OS desktop where the project will be generated and compiled
+
+RD /S /Q "%DESKTOP_PATH%\site_php-dev"
+
+mkdir "%DESKTOP_PATH%\site_php-dev"
+
+cd "%DESKTOP_PATH%\site_php-dev"
+
+
+:: Generate the site_php project and build all the turbo depedencies
+
+call tb -g site_php
+
+call npm ci
+
+set PROJECT_DIR=%cd%
+
+cd "%TURBOSITE_PHP_PATH%"
+
+call tb -cb 
+
+cd "%TURBOCOMMONS_PHP_PATH%"
+
+call tb -cb 
+
+cd "%TURBODEPOT_PHP_PATH%"
+
+call tb -cb 
+
+cd %PROJECT_DIR%
+
+
+:: Copy all the turbo dependencies to the generated project libs folder
+
+copy "%TURBOCOMMONS_PHP_PATH%\target\turbocommons-php\dist\*.*" "src\main\libs\turbocommons-php"
+copy "%TURBOSITE_PHP_PATH%\target\turbosite-php\dist\*.*" "src\main\libs\turbosite-php"
+copy "%TURBODEPOT_PHP_PATH%\target\turbodepot-php\dist\*.*" "src\main\libs\turbodepot-php"
+
+md "%WEB_SERVER_DEV_FOLDER_PATH%"
+
+
+:: Build the project and run the tests
+
+call tb -cbst
+
+pause
