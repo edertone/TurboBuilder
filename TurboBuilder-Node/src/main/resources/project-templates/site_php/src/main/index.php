36fb3b1ea8d202e77971056059a0fb934255f09f
Jaume Mussons <jaumemussons79@gmail.com>
2021-12-20 15:50:14 +0100

    NA: Updated project dependencies

--- TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/index.php
+++ TurboBuilder-Node/src/main/resources/project-templates/site_php/src/main/index.php
@@ -11,7 +11,7 @@ if(file_exists($cacheFile = __DIR__.DIRECTORY_SEPARATOR.'___views_cache___'.DIRE
 }
 
 require 'phar://libs/turbosite-php/turbosite-php-8.2.0.phar/php/autoloader-project.php';
-require 'libs/turbocommons-php/turbocommons-php-3.7.0.phar';
+require 'libs/turbocommons-php/turbocommons-php-3.8.0.phar';
 require 'libs/turbodepot-php/turbodepot-php-7.0.1.phar';
 require 'libs/turbosite-php/turbosite-php-8.2.0.phar';
 
