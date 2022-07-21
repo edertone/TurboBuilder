6f5aaa66910bf51a38e2d3d99bfe7a3e0fe6ea67
Jaume Mussons <jaumemussons79@gmail.com>
2022-07-21 21:01:51 +0200

    NA: Updated project dependencies

--- TurboBuilder-Node/extras/bats/site_php-build-and-test-template.bat
+++ TurboBuilder-Node/extras/bats/site_php-build-and-test-template.bat
@@ -3,16 +3,16 @@
 :: Modify it to fit your needs
 
 
-@SET DESKTOP_PATH=%USERPROFILE%\Desktop
-@SET TURBOSITE_PHP_PATH=%USERPROFILE%\git\TurboSite\TurboSite-Php
-@SET TURBOCOMMONS_PHP_PATH=%USERPROFILE%\git\TurboCommons\TurboCommons-Php
-@SET TURBODEPOT_PHP_PATH=%USERPROFILE%\git\TurboDepot\TurboDepot-Php
+@SET DESKTOP_PATH=D:\Storage\Desktop
+@SET TURBOSITE_PHP_PATH=D:\Storage\Backups\Git\TurboSite\TurboSite-Php
+@SET TURBOCOMMONS_PHP_PATH=D:\Storage\Backups\Git\TurboCommons\TurboCommons-Php
+@SET TURBODEPOT_PHP_PATH=D:\Storage\Backups\Git\TurboDepot\TurboDepot-Php
 @SET WEB_SERVER_DEV_FOLDER_PATH=C:\turbosite-webserver-symlink\_dev
 
 
 :: Create a work directory into the OS desktop where the project will be generated and compiled
 
-RD /S /Q "%DESKTOP_PATH%\site_php-dev"
+if exist "%DESKTOP_PATH%\site_php-dev" RD /S /Q "%DESKTOP_PATH%\site_php-dev"
 
 mkdir "%DESKTOP_PATH%\site_php-dev"
 
@@ -25,33 +25,41 @@ call tb -g site_php
 
 call npm ci
 
-set PROJECT_DIR=%cd%
+echo BUILD TURBOSITE ------------------------------------------------------------
 
 cd "%TURBOSITE_PHP_PATH%"
 
 call tb -cb 
 
+echo BUILD TURBOCOMMONS ---------------------------------------------------------
+
 cd "%TURBOCOMMONS_PHP_PATH%"
 
 call tb -cb 
 
+echo BUILD TURBODEPOT -----------------------------------------------------------
+
 cd "%TURBODEPOT_PHP_PATH%"
 
-call tb -cb 
-
-cd %PROJECT_DIR%
+call tb -cb
 
 
 :: Copy all the turbo dependencies to the generated project libs folder
+
+echo COPY DEPENDENCIES -----------------------------------------------------------
+
+cd "%DESKTOP_PATH%\site_php-dev"
 
 copy "%TURBOCOMMONS_PHP_PATH%\target\turbocommons-php\dist\*.*" "src\main\libs\turbocommons-php"
 copy "%TURBOSITE_PHP_PATH%\target\turbosite-php\dist\*.*" "src\main\libs\turbosite-php"
 copy "%TURBODEPOT_PHP_PATH%\target\turbodepot-php\dist\*.*" "src\main\libs\turbodepot-php"
 
-md "%WEB_SERVER_DEV_FOLDER_PATH%"
+if not exist "%WEB_SERVER_DEV_FOLDER_PATH%" mkdir "%WEB_SERVER_DEV_FOLDER_PATH%"
 
 
 :: Build the project and run the tests
+
+echo BUILD AND RUN TESTS -----------------------------------------------------------
 
 call tb -cbst
 
